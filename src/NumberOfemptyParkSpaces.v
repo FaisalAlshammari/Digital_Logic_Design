1b454cb3821d18d36268146747e896226954f1ab
Faisal <106087487+FaisalAlshammari@users.noreply.github.com>
2022-05-25 13:17:33 +0300

    Delete src directory

--- src/NumberOfemptyParkSpaces.v
+++
@@ -1,8 +0,0 @@
-module NumberOfemptyParkSpaces(car1,car2,car3,numOfParks); 
-	
-	input car1, car2, car3;
-	output [1:0] numOfParks ;
-	
-	assign numOfParks = car1 + car2 + car3;
-	
-endmodule
--- src/couterTest.v
+++
@@ -1,7 +0,0 @@
-module testCount(input clk, output reg [0:2] bus, input reset);
-	always @(posedge clk,posedge reset)
-	begin
-		if(reset)bus =0;
-		else bus = bus+1;		 
-	end 
-	endmodule
